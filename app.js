a8fa6f94fb4826bb08c887c025a25b2c29a3a967
Magyar Tamás <tamas.donix@gmail.com>
2020-08-08 18:13:41 +0200

    resoult template added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const level = require('./routes/level')
 const version = require('./routes/version')
 const player = require('./routes/palyer')
 const name = require('./routes/name')
+const resoult = require('./routes/resoult')
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,5 +17,6 @@ app.use('/version', version)
 app.use('/levels', level)
 app.use('/player', player)
 app.use('/name', name)
+app.use('/resoult', resoult)
 
 app.listen(PORT, () => console.log("Server started. " + PORT))
---
+++ routes/resoult.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const db = require('../database')
+var router = express.Router()
+
+
+router.post('/', function (req, res) 
+{
+
+    
+
+})
+
+module.exports = router
