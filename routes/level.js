9095eb5d4a192211dd4279d4dff13dfa76184916
Magyar Tamás <tamas.donix@gmail.com>
2020-10-06 13:10:24 +0200

    new levels added

--- routes/level.js
+++ routes/level.js
@@ -4,157 +4,134 @@ var router = express.Router()
 var Levels =
 [
     {
-        color: "red",
+        color: "green",
         levels:
         [
             {
                 id: 0,
                 unlocks: [],
-                name: "First Job",
+                name: "A fresh start"
                 reward_wood: 50,
-                reward_iron: 10,
-                boxes: [0,0,0,0,0],
-                maxlost: 0
-            },
-            {
-                id: 1,
-                unlocks: [],
-                name: "Sec Job",
-                reward_wood: 50,
-                reward_iron: 50,
-                boxes: [0,0,1,1,0,0],
+                reward_iron: 20,
+                boxes: [0, 0, 0, 0, 0, 0],
                 maxlost: 1
             }
         ]
-
     },
+    
     {
-        color: "green",
+        color: "yellow",
         levels:
         [
             {
-                id: 2,
+                id: 1,
                 unlocks: [0],
-                name: "third Job",
-                reward_wood: 520,
-                reward_iron: 80,
-                boxes: [0,0,2,1,1],
+                name: "Things start to work"
+                reward_wood: 50,
+                reward_iron: 25,
+                boxes: [1, 1, 0, 0, 0],
                 maxlost: 0
             },
             {
+                id: 2,
+                unlocks: [0],
+                name: "Heavy supplies"
+                reward_wood: 10,
+                reward_iron: 40,
+                boxes: [1, 1, 1, 1, 1, 1, 1],
+                maxlost: 1
+            },
+        ]
+    },
+    
+    {
+        color: "orange",
+        levels:
+        [
+            {
                 id: 3,
                 unlocks: [1],
-                name: "fourth Job",
-                reward_wood: 50,
-                reward_iron: 600,
-                boxes: [1,1,1,1,1,1],
+                name: "Bouncy"
+                reward_wood: 60,
+                reward_iron: 30,
+                boxes: [0, 0, 0, 0, 0, 0, 3, 3, 3, 3],
                 maxlost: 1
             },
             {
                 id: 4,
-                unlocks: [1],
-                name: "fifth Job",
-                reward_wood: 510,
-                reward_iron: 100,
-                boxes: [2,2,2,2,2,2,2,2,2,2,2],
-                maxlost: 3
-            }
+                unlocks: [2],
+                name: "Important supplements"
+                reward_wood: 70,
+                reward_iron: 10,
+                boxes: [0, 0, 1, 1, 1, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0],
+                maxlost: 2
+            },
         ]
     },
+
     {
         color: "red",
         levels:
         [
             {
                 id: 5,
-                unlocks: [4],
-                name: "fifth Job",
-                reward_wood: 510,
-                reward_iron: 100,
-                boxes: [1,2,1,2,1,2,1,2,1,2,1],
-                maxlost: 3
-            }
+                unlocks: [3, 4],
+                name: "Small, but expensive"
+                reward_wood: 60,
+                reward_iron: 40,
+                boxes: [3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2],
+                maxlost: 0
+            },
         ]
     },
+
     {
-        color: "red",
+        color: "blue",
         levels:
         [
             {
                 id: 6,
                 unlocks: [5],
-                name: "fifth Job",
-                reward_wood: 510,
-                reward_iron: 100,
-                boxes: [1,1,1,0,0,0,2,2,1,0,2],
-                maxlost: 3
-            }
+                name: "Tower"
+                reward_wood: 50,
+                reward_iron: 25,
+                boxes: [3, 3, 5, 5, 3, 3, 5, 5],
+                maxlost: 0
+            },
+            {
+                id: 7,
+                unlocks: [5],
+                name: "For constuction... and a few tires"
+                reward_wood: 60,
+                reward_iron: 70,
+                boxes: [3, 3, 5, 1, 5, 1],
+                maxlost: 1
+            },
         ]
     },
+
     {
-        color: "red",
+        color: "purple",
         levels:
         [
             {
-                id: 7,
-                unlocks: [6],
-                name: "fifth Job",
-                reward_wood: 510,
-                reward_iron: 100,
-                boxes: [1,1,1,0,0,0,2,2,1,0,2],
-                maxlost: 3
-            },
-            {
                 id: 8,
                 unlocks: [6],
-                name: "fifth Job",
-                reward_wood: 510,
+                name: "Car Enterprise"
+                reward_wood: 100,
                 reward_iron: 100,
-                boxes: [1,1,1,1,1,1,1,1,1,1,2],
+                boxes: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
                 maxlost: 3
             },
             {
                 id: 9,
-                unlocks: [6],
-                name: "fifth Job",
-                reward_wood: 510,
+                unlocks: [7],
+                name: "Big Deal"
+                reward_wood: 100,
                 reward_iron: 100,
-                boxes: [1,1,1,0,0,0,2,2,1,0,2],
-                maxlost: 3
+                boxes: [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5],
+                maxlost: 1
             },
-            {
-                id: 10,
-                unlocks: [6],
-                name: "fifth Job",
-                reward_wood: 510,
-                reward_iron: 100,
-                boxes: [1,2,2,2,2,2,2,2,1,0,2],
-                maxlost: 3
-            }
-        ]
-    },
-    {
-        color: "red",
-        levels:
-        [
-            {
-                id: 11,
-                unlocks: [7,8,9,10],
-                name: "fifth Job",
-                reward_wood: 510,
-                reward_iron: 100,
-                boxes: [1,1,1,0,0,0,2,2,1,0,2,1],
-                maxlost: 3
-            },
-            {
-                id: 12,
-                unlocks: [7,8,9,10],
-                name: "blabla Job",
-                reward_wood: 999,
-                reward_iron: 999,
-                boxes: [3,3,3,4,4,4,5,5,5],
-                maxlost: 2
-            }
         ]
     }
 ]
