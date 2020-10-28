19b938ff12ae5334cdcdba86f6bb319960958175
Magyar Tamás <tamas.donix@gmail.com>
2020-10-28 19:24:34 +0100

    invite status request added

--- routes/invite.js
+++ routes/invite.js
@@ -4,6 +4,7 @@ const sha = require('sha1');
 var router = express.Router()
 
 const PLAYSTORE_URL = "https://play.google.com/store/apps/details?id=com.donix.logistix";
+const PREMIUM_UNLOCK = 200;
 
 router.get('/:name', function (req, res) 
 {
@@ -17,13 +18,25 @@ router.get('/:name', function (req, res)
 
 router.get('/', (_, res) => res.redirect(PLAYSTORE_URL));
 
-router.post('/', function (req, res) 
+router.post('', function (req, res) 
 {
-    var mail = req.body.email;
 
-    
-    //TODO: return unique invites
+    if(req.body.email)
+    {
+        console.log(`[Invite] ${req.body.email} requested the invite status.`)
+        db.query(countPlayerInvites(req.body.email), 
+        (data) => 
+        {
+            let n = 0;
 
+            if(data.rows.length > 0)
+                n = parseInt(data.rows[0].count);
+
+            res.send(ceateResoult(n));
+        })
+    }
+    else
+        res.status(401).send("You must give the email field.");
 })
 
 function hashIP(req)
@@ -36,6 +49,22 @@ function addRefer(name, ip)
     return {    
         text: 'INSERT INTO invites (inviter, iphash, date) VALUES ($1, $2, $3)',
         values: [name, ip, new Date()]
+    }
+}
+
+function ceateResoult(c)
+{
+    return {
+        count: c,
+        reward: PREMIUM_UNLOCK
+    }
+}
+
+function countPlayerInvites(mail)
+{
+    return {    
+        text: 'SELECT count(DISTINCT iphash) from invites, users WHERE email = $1 AND inviter = name GROUP BY inviter',
+        values: [mail]
     }
 }
 
