490365bd999ab712ed7b4c583abc99730bd18f9d
Magyar Tamás <tamas.donix@gmail.com>
2020-10-29 16:25:30 +0100

    settings moved to settings.js

---
+++ settings.js
@@ -0,0 +1,11 @@
+const settings =
+{
+    VERSION: { "api" : "1.0", "client" : "1.0" },
+    PLAYSTORE_URL: "https://play.google.com/store/apps/details?id=com.donix.logistix",
+    PLAYSTORE_IMG: "https://play.google.com/store/apps/details?id=com.donix.logistix",
+    INVITE_TITLE: "Start playing Logistix now!",
+    INVITE_DESC: "Your friend already playing, join to beat his/her records.",
+    PREMIUM_UNLOCK: 200,
+}
+
+module.exports = settings;
