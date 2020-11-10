a9cc047e393534a725398201263b2e8e69c5be61
Magyar Tamás <tamas.donix@gmail.com>
2020-11-10 22:30:00 +0100

    main panel info added

--- Assets/Script/Menu/Panels/MainPanel.cs
+++ Assets/Script/Menu/Panels/MainPanel.cs
@@ -1,9 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
 
 class MainPanel : MenuPanel
 {
 
-	protected override void OnOpen() {}
+	[SerializeField] private Text Name;
+	[SerializeField] private Text Wood;
+	[SerializeField] private Text Iron;
+
+	protected override void OnOpen() 
+	{
+		PlayerModel model = Player.Instance.GetModel();
+		Name.text = model.Name;
+		Wood.text = ToReadableNumber(model.Wood);
+		Iron.text = ToReadableNumber(model.Iron);
+	}
 
 	protected override void OnClose() {}
 
@@ -14,6 +27,37 @@ class MainPanel : MenuPanel
 		Menu.Instance.Swich(Menu.Instance.Levels);
 	}
 
+	public void OnPremiumButtonClick()
+	{
+		//TODO
+	}
+
+	public void OnShopButtonClick()
+	{ 
+		//TODO
+	}
+
+	#endregion
+
+	#region PRIAVTE
+
+	private static string ToReadableNumber(int number)
+	{
+		string Reverse(string str) { return new string(str.Reverse().ToArray()); }
+
+		StringBuilder tmp = new StringBuilder();
+
+		int c = 0;
+		foreach(char chr in Reverse(number.ToString()))
+		{
+			if (c % 3 == 0 && c > 0)
+				tmp.Append('.');
+			tmp.Append(chr);
+			c++;
+		}
+
+		return Reverse(tmp.ToString());
+	}
 
 	#endregion
 
