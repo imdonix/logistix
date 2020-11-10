3d4ae386ebe3701d7a4af6c86e78e43fde8bae1e
Magyar Tamás <tamas.donix@gmail.com>
2020-11-11 00:22:43 +0100

    premium functionaly added to MainPanel

--- Assets/Script/Menu/Panels/MainPanel.cs
+++ Assets/Script/Menu/Panels/MainPanel.cs
@@ -9,13 +9,16 @@ class MainPanel : MenuPanel
 	[SerializeField] private Text Name;
 	[SerializeField] private Text Wood;
 	[SerializeField] private Text Iron;
+	[SerializeField] private GameObject PremiumButton;
 
 	protected override void OnOpen() 
 	{
 		PlayerModel model = Player.Instance.GetModel();
 		Name.text = model.Name;
+		Name.color = model.Premium ? Color.yellow : Color.white;
 		Wood.text = ToReadableNumber(model.Wood);
 		Iron.text = ToReadableNumber(model.Iron);
+		PremiumButton.SetActive(!model.Premium);
 	}
 
 	protected override void OnClose() {}
