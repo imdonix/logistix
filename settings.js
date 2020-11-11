b25678c9d0f6a821170d47907da9f05ebf203d74
Magyar Tamás <tamas.donix@gmail.com>
2020-11-11 10:38:14 +0100

    fixed a bug with premium unlock

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings =
     PLAYSTORE_IMG: process.env.PLAYSTORE_IMG    || "/",
     INVITE_TITLE: process.env.INVITE_TITLE      || "Play now!",
     INVITE_DESC: process.env.INVITE_DESC        || "",
-    PREMIUM_UNLOCK: process.env.REMIUM_UNLOCK  || 50,
+    PREMIUM_UNLOCK: process.env.PREMIUM_UNLOCK  || 50,
 }
 
 module.exports = settings;
