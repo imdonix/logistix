a7209b22539e141ed586521a5cd6e7161276ac91
Magyar Tamás <tamas.donix@gmail.com>
2020-11-11 21:29:21 +0100

    added Item logic

---
+++ Assets/Script/Logic/Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class Item : MonoBehaviour
+{
+
+    private Rigidbody2D Rigid;
+    private Collider2D Collider;
+
+    private void Awake()
+    {
+        Rigid = GetComponent<Rigidbody2D>();
+        Collider = GetComponent<Collider2D>();
+    }
+
+
+    #region PUBLIC
+
+    public virtual bool IsLost()
+    {
+        return transform.position.y < Game.WATER_LEVEL;
+    }
+
+    #endregion
+
+}
