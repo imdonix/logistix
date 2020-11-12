cf96da22eb0c8b713203d8d2d24a61689af5effd
Magyar Tamás <tamas.donix@gmail.com>
2020-11-12 18:32:29 +0100

    added bullet

---
+++ Assets/Script/Items/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Serialization;
+using UnityEngine;
+
+public class Bullet : Item
+{
+    [SerializeField] private float force;
+    [SerializeField] private float explosion;
+
+    #region UNITY
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameManager.Instance.SpawnExplosion(collision.transform.position, explosion);
+        Destroy(gameObject);
+    }
+
+    #endregion
+
+
+    #region PUBLIC
+    public void Fire()
+    {
+        GetRigidbody2D().AddForce((transform.rotation * Vector2.right) * force);
+    }
+
+    #endregion
+
+
+
+}
+
