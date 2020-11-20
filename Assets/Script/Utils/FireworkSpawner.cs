6653217d908ce8ab92f606249b4831b76829a389
Magyar Tamás <tamas.donix@gmail.com>
2020-11-20 22:59:53 +0100

    Firework spawner added

---
+++ Assets/Script/Utils/FireworkSpawner.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireworkSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] Fireworks;
+    [SerializeField] private int Count;
+    [SerializeField] private float Sway;
+    [SerializeField] private float Delay;
+
+    private bool active;
+
+    private void Start() { active = false; }
+
+    #region PUBLIC
+
+    public void Spawn()
+    {
+        active = true;
+        for (int i = 0; i < Count; i++)
+            StartCoroutine(Spawner());
+    }
+
+    public void Stop()
+    {
+        active = false;
+    }
+
+    #endregion
+
+    private IEnumerator Spawner() 
+    {
+        GameObject firework = null;
+
+        void Delete()
+        {
+            if (!ReferenceEquals(firework, null)) Destroy(firework);
+        }
+
+        while (active)
+        {
+            Delete();
+            firework = Instantiate(GetRandomFirework(), transform.position + GetRandomDelta(), Quaternion.identity);
+            yield return new WaitForSeconds(3 + Random.Range(0, Delay));
+        }
+        Delete();
+    }
+
+    private GameObject GetRandomFirework()
+    {
+        return Fireworks[Random.Range(0, Fireworks.Length)];
+    }
+
+    private Vector3 GetRandomDelta()
+    {
+        return new Vector2(Random.Range(-Sway, Sway), Random.Range(-Sway, Sway));
+    }
+
+}
