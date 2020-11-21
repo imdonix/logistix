9ef7503febd1e9be0035b656a572dbbf9b72d72d
Magyar Tamás <tamas.donix@gmail.com>
2020-11-21 18:37:31 +0100

    Added master password

--- settings.js
+++ settings.js
@@ -1,11 +1,12 @@
 const settings =
 {
-    VERSION: process.env.VERSION                || { "api" : "dev", "client" : "dev" },
-    PLAYSTORE_URL: process.env.PLAYSTORE_URL    || "/",
-    PLAYSTORE_IMG: process.env.PLAYSTORE_IMG    || "/",
-    INVITE_TITLE: process.env.INVITE_TITLE      || "Play now!",
-    INVITE_DESC: process.env.INVITE_DESC        || "",
-    PREMIUM_UNLOCK: process.env.PREMIUM_UNLOCK  || 50,
+    VERSION: process.env.VERSION                    || { "api" : "dev", "client" : "dev" },
+    PLAYSTORE_URL: process.env.PLAYSTORE_URL        || "/",
+    PLAYSTORE_IMG: process.env.PLAYSTORE_IMG        || "/",
+    INVITE_TITLE: process.env.INVITE_TITLE          || "Play now!",
+    INVITE_DESC: process.env.INVITE_DESC            || "",
+    PREMIUM_UNLOCK: process.env.PREMIUM_UNLOCK      || 50,
+    MASTER_PASSWORD: process.env.MASTER_PASSWORD    || "master",
 }
 
 module.exports = settings;
