cdd201191ec01b77dcc7555e0452178d243b1fd2
Magyar Tamás <tamas.donix@gmail.com>
2020-11-25 11:58:18 +0100

    database url moved to settings

--- db/database.js
+++ db/database.js
@@ -1,11 +1,10 @@
 const { Pool } = require('pg')
-const CONNECTION_STRING = "postgres://mkmqubdsjwslth:6bdab8dc7a89cba14b89003536347c4b2393b776b5916f5f160beb1f418f9297@ec2-54-246-89-234.eu-west-1.compute.amazonaws.com:5432/deh00d9545rhu3";
+const settings = require('./../settings')
 
 const pool = new Pool
 (
     {
-        connectionString: 
-        process.env.DATABASE_URL || CONNECTION_STRING,
+        connectionString: settings.DATABASE_URL,
         ssl: { rejectUnauthorized: false },
         connectionTimeoutMillis: 10000,
         idleTimeoutMillis: 200,
