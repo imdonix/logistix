8881e5c805a2793b89b43c9fc2794621a272af2d
Magyar Tamás <tamas.donix@gmail.com>
2020-11-26 20:58:03 +0100

    added bug post template

--- routes/bug.js
+++ routes/bug.js
@@ -5,4 +5,11 @@ var router = express.Router()
 router.get('/', (req,res) => res.sendFile(path.join(__dirname + '/../public/bug.html')))
 router.get('/script.js', (req,res) => res.sendFile(path.join(__dirname + '/../public/bug.js')))
 
+router.post('/', (req,res) =>
+{
+    res.status(200).send();
+    console.log(req.body) //TODO
+
+})
+
 module.exports = router
