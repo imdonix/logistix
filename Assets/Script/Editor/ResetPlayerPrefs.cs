3dc604ddb7ab8bcb0998862d3cb30fee83f74ce9
Magyar Tamás <tamas.donix@gmail.com>
2020-11-26 11:04:56 +0100

    PlayerPrefs reset menu item added

---
+++ Assets/Script/Editor/ResetPlayerPrefs.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ResetPlayerPrefs
+{
+    [MenuItem("PlayerPrefs/Reset")]
+    static void Reset()
+    {
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+    }
+}
