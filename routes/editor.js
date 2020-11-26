4b64d60627933e0c797cf9786d561e15686cd31c
Magyar Tamás <tamas.donix@gmail.com>
2020-11-26 18:12:07 +0100

    script.js -> editor.js

--- routes/editor.js
+++ routes/editor.js
@@ -3,6 +3,6 @@ const path = require('path')
 var router = express.Router()
 
 router.get('/', (req,res) => res.sendFile(path.join(__dirname + '/../public/editor.html')))
-router.get('/script.js', (req,res) => res.sendFile(path.join(__dirname + '/../public/script.js')))
+router.get('/script.js', (req,res) => res.sendFile(path.join(__dirname + '/../public/editor.js')))
 
 module.exports = router
