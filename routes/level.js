f124107df3417da56144152e243c81d90e25a17b
Magyar Tamás <tamas.donix@gmail.com>
2020-11-27 12:13:00 +0100

    cyrb53 moved to a crypto modul

--- routes/level.js
+++ routes/level.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const settings = require('../settings')
 const db = require('../db/database')
+const { cyrb53 } = require('../crypto')
 var router = express.Router()
 
 router.get('/', function (req, res) 
@@ -30,19 +31,5 @@ router.post('/', function (req,res)
     else
         res.status(403).send("Bad password!");
 })
-
-function cyrb53(str) 
-{
-    let h1 = 0xdeadbeef ^ 14, h2 = 0x41c6ce57 ^ 14;
-    for (let i = 0, ch; i < str.length; i++) 
-    {
-        ch = str.charCodeAt(i);
-        h1 = Math.imul(h1 ^ ch, 2654435761);
-        h2 = Math.imul(h2 ^ ch, 1597334677);
-    }
-    h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
-    h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
-    return 4294967296 * (2097151 & h2) + (h1>>>0);
-};
 
 module.exports = router
