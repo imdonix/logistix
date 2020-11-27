be61c83b9e446093f07a1a2f5d42744850dde6ab
Magyar Tamás <tamas.donix@gmail.com>
2020-11-27 11:59:14 +0100

    removed text length indicator

--- public/bug.js
+++ public/bug.js
@@ -6,7 +6,6 @@ const device = document.querySelector('#device')
 const ram = document.querySelector('#ram')
 const player = document.querySelector('#player')
 const bug = document.querySelector('#bug')
-const size = document.querySelector('#size')
 const send = document.querySelector('#send')
 
 const url = new URLSearchParams(window.location.search);
@@ -16,7 +15,6 @@ const max = 200
 function init()
 {
     send.addEventListener('click', sendReport)
-    bug.addEventListener('keyup', checkBugReportSize)
 
     const player = url.get('player');
     if(player)
@@ -29,32 +27,6 @@ function init()
     {
         report.classList.toggle('hidden', true);
         main.classList.toggle('hidden', false);
-    }
-}
-
-function checkBugReportSize()
-{
-    const textSize = bug.value.length
-    if(textSize < min)
-    {
-        size.innerHTML = `${min - textSize} more`
-        size.classList.toggle('badge-secondary', true)
-        size.classList.toggle('badge-warning', false)
-        size.classList.toggle('badge-success', false)
-    }
-    else if(textSize > max)
-    {
-        size.innerHTML = `to long`
-        size.classList.toggle('badge-warning', true)
-        size.classList.toggle('badge-secondary', false)
-        size.classList.toggle('badge-success', false)
-    }
-    else
-    {
-        size.innerHTML = `${textSize}`
-        size.classList.toggle('badge-success', true)
-        size.classList.toggle('badge-warning', false)
-        size.classList.toggle('badge-secondary', false)
     }
 }
 
@@ -83,13 +55,13 @@ function swichToThank()
 
 function sendReport()
 {
-
     fetch('/bug', 
         {
             method: 'POST', 
             body: JSON.stringify(getData()),
             headers: {'Content-Type': 'application/json'}
         }).then(res => console.log(`Bug sent (${res.status})`))
+
     swichToThank();
 }
 
