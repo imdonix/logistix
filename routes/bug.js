a6e38c2dde67d1225894c36d7885b0e50416daa5
Magyar Tamás <tamas.donix@gmail.com>
2020-11-27 12:13:27 +0100

    added get all bugs

--- routes/bug.js
+++ routes/bug.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const db = require('../db/database')
+const { cyrb53 } = require('../crypto')
 var router = express.Router()
 
 router.get('/', (req,res) => res.sendFile(path.join(__dirname + '/../public/bug.html')))
@@ -13,5 +14,16 @@ router.post('/', (req,res) =>
     if(data.send)
         db.query(db.postBug(data.send, data.player, data.device, data.ram), (_) => console.log("[Bug] posted"))
 })
+
+router.post('/all', (_,res) =>
+{
+    console.log("[Bug] requested all")
+    db.query(db.getBugs(), (data) =>
+    {
+        res.status(200).send(data.rows);
+    })
+})
+
+
 
 module.exports = router
